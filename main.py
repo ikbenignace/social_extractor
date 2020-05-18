287240180fc47f69d8a98931a895dbdc4c6d2f2c
quinten-bosch <qpro1234gamer@gmail.com>
2020-05-18 23:11:01 +0200

    Update

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #Main file
 #Zip Selector
 #Invoke script for selected platform
+
+
+def snap(): #Start the snapchat script
+
+    print('test')
+    exec(open("snap.py").read())
+
+
+while True:
+    platform_selector = input('Enter the number of the platform.. ')
+
+    if int(platform_selector) == 1:
+        snap()
+
